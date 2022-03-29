d479c396d3e1da57aac38c801ecb30947eea975f
21ci0140hosoya <21ci0140@jec.ac.jp>
2022-03-29 14:04:31 +0900

    スライムの大きさが変化するアイテムの製作

---
+++ GAMETAISYOU/Assets/Hosoya/item/script/SlimeSizeChange_Item.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeSizeChange_Item : MonoBehaviour
+{
+    [SerializeField] float chengeSize;  //‘å‚«‚³‚Ì•Ï‰»—Ê
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //ƒXƒ‰ƒCƒ€‚¾‚Á‚½‚ç
+        if(GameObject.FindWithTag("Slime"))
+        {
+            collision.gameObject.GetComponent<SlimeController>().scale += chengeSize;
+            Destroy(this.gameObject);
+        }
+    }
+}
