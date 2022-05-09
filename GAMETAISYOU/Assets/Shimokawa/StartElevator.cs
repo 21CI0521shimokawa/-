99c78682bd333343b55ee003912ae9aa829c377a
MSI\kazus <21ci0521@jec.ac.jp>
2022-05-09 12:29:11 +0900

    stage1-1&エレベーター本モデル移行

--- GAMETAISYOU/Assets/Shimokawa/StartElevator.cs
+++ GAMETAISYOU/Assets/Shimokawa/StartElevator.cs
@@ -4,43 +4,48 @@ using UnityEngine;
 
 public class StartElevator : MonoBehaviour
 {
+    private Vector3 pos;
+    [SerializeField, Tooltip("Yの位置")]
+    private float DestinationPosY;
+    public Vector3 DestinationPos;
+    public bool is2ndFloor;
 
-    [SerializeField, Tooltip("現在の位置")]
-    private Vector3 NowPosition;
-
-    [SerializeField, Tooltip("目標位置")]
-    private GameObject Destination;
-
-    [SerializeField, Tooltip("Elevatorのスピード")]
-    private float Speed;
-
-
-    //当たり判定のすり抜け防止
-    public Rigidbody2D ElevatorRigidbody;
     void Start()
     {
-        ElevatorRigidbody = GetComponent<Rigidbody2D>();
-
-       // DestinationNum = Mathf.Abs(Destination.transform.position.y);
-       // StartCoroutine("ElevatorUp");
-       // Debug.Log(DestinationNum);
+        DestinationPos = new Vector3(0, DestinationPosY, 0);
+        pos = this.transform.position;
+        is2ndFloor = false;
+        MoveUp();
     }
-    private void Update()
+    public void MoveUp()
     {
-        transform.position = Vector3.MoveTowards(transform.position,Destination.transform.position, Speed*Time.deltaTime);
+        StartCoroutine("MoveUpStart");
     }
-    #region コルーチン
-    /* public IEnumerator ElevatorUp()
-     {
-         while (NowPosition.y < DestinationNum)
-         {
-             NowPosition = transform.position;
-             ElevatorRigidbody.velocity = new Vector2(0, Speed);
-             yield return new WaitForSeconds(0.01f);
-         }
-         ElevatorRigidbody.bodyType = RigidbodyType2D.Static;
-         yield return new WaitForSeconds(0.01f);
-         yield break;
-     }*/
-    #endregion
+
+    IEnumerator MoveUpStart()
+    {
+        while (pos.y < DestinationPos.y)
+        {
+            pos = transform.position;
+            transform.Translate(0, 0.02f, 0);
+            yield return new WaitForSeconds(0.01f);
+        }
+        is2ndFloor = true;
+    }
+    public void MoveDown()
+    {
+        StartCoroutine("MoveDownStart");
+
+    }
+
+    IEnumerator MoveDownStart()
+    {
+        while (pos.y > 0.0f)
+        {
+            pos = transform.position;
+            transform.Translate(0, -0.02f, 0);
+            yield return new WaitForSeconds(0.01f);
+        }
+        is2ndFloor = false;
+    }
 }
