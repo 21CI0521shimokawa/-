81e5af57db0fdf845c8d994ca656d5732c8ae3cb
21ci0140hosoya <21ci0140@jec.ac.jp>
2022-05-12 01:19:12 +0900

    シャッターとレバー追加

---
+++ GAMETAISYOU/Assets/Hosoya/Gimmick/Shutter/Shutter/ShutterController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShutterController : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void _Open()
+    {
+        animator.SetTrigger("ShutterOpen");
+        audioSource.Play();
+    }
+}
