48d743f256e13029bc32070de2ddef977d98a6bc
MSI\kazus <21ci0521@jec.ac.jp>
2022-10-21 11:57:02 +0900

    スクリプトブラッシュVer.4

---
+++ GAMETAISYOU/Assets/Shimokawa/Script/GoalController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalController : MonoBehaviour
+{
+    [SerializeField] PlayBGM PlayBGM;
+    [SerializeField] float FadeTime;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Slime")
+        {
+            FadeManager.Instance.LoadScene("GameClear", FadeTime); //GameClearシーンに移行
+            PlayBGM._FadeOutStart(); //BGMをフェードアウトさせる
+            collision.gameObject.GetComponent<SlimeController>()._ifOperation = false; //ゴール地点に到達したらスライムの行動を止める
+        }
+    }
+}
